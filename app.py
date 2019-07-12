8a0b2a697a35cb43c48cdb13240f1a6a637a2a1f
caspii <caspar.wrede@gmail.com>
2019-07-12 15:06:32 +0200

    Amazing

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from random import randrange
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +8,15 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/goodbye')
-def goodbye():
-    return 'Goodbye world'
+@app.route('/form')
+def form():
+    return render_template('form.html', name="Donald")
+
+
+@app.route('/result', methods=['get', 'post'] )
+def result():
+
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    print("Firstname = " + firstname)
+    return "Thank you! " + firstname
